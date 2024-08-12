3022862e18b8d0d04891a77765fabf5cf5cc2f3f
rayssassousa22 <rayssa.sousa78@outlook.com>
2024-08-12 15:43:43 -0300

    fetching a server response

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,33 @@
-const limit = 15; //limite de respostas que eu quero
-const offset = 0; //paginação de pokemons
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=' + offset + '&limit=' + limit; //endereço da qual estou tirando minhas respostas
+const limit = 15; 
+const offset = 0; 
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=' + offset + '&limit=' + limit; 
+
+
+// faz aquela estrutura html dos cards
+function convertPokemonDetails(pokemon){
+    return '<li class="pokemon"> <p>' + pokemon.name + '</p><div class="details"><ol class="pokemon-type"><li class="type">Fire</li></ol><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg" alt="Charizard"></div></li>';
+} 
+
+const pokemonList = document.getElementById('cards');
+
+/*innerHTML x appendChild
+
+o innerhtml trata como texto
+appendChild trata como objeto */
 
 fetch(url ) //função pra pedir isso tudo e retornar minha respostas
 
-    .then((response) => response.json()) //isso é uma arrowfunction, simplificação do encadeamento fetch
-    .then((jsonBody) => console.log(jsonBody))
+    .then((response) => response.json()) 
+    .then((jsonBody) => jsonBody.results) //devolve apenas os resultados e passa pro próximo then as caractérisitica de cada pokemon
+    .then((pokemons) => {
+        
+        //percorre cada pokemon do meu limite substituindo as informações
+        for(let i = 0; i < pokemons.length; i++){
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonDetails(pokemon);
+        }
+    })
     .catch((error) => console.log(error))
     .finally(function(){
         console.log('concluído');
     }); 
-
-//esse é o processo de requisição pra um servidor e ele me dando uma resposta, e interface básica de requisição de informações
