dbad35291e64ab560b8daf4f87141667308c361b
rayssassousa22 <rayssa.sousa78@outlook.com>
2024-08-12 15:11:38 -0300

    fetching a server response

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,18 +2,12 @@ const limit = 15; //limite de respostas que eu quero
 const offset = 0; //paginação de pokemons
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=' + offset + '&limit=' + limit; //endereço da qual estou tirando minhas respostas
 
-fetch(url) //função pra pedir isso tudo e retornar minha respostas
+fetch(url ) //função pra pedir isso tudo e retornar minha respostas
 
-    .then(function (response){ // para evitar encadeamento esse then pega a resposta
-        return response.json();
-    })
-    .then(function(jsonBody){ //esse then recebe a resposta e converte em modificável
-        console.log(jsonBody);
-    })
-    .catch(function (error) {//funçãopara caso dar errado
-        console.log(error);
-    })
-    .finally(function(){ //função final que irá ocorrer independente da resposta da promise
+    .then((response) => response.json()) //isso é uma arrowfunction, simplificação do encadeamento fetch
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
+    .finally(function(){
         console.log('concluído');
     }); 
 
