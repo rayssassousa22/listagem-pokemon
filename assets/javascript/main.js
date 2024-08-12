d76cba439908e3f62ab2c06b3b6ef68f7bf9aaa8
rayssassousa22 <rayssa.sousa78@outlook.com>
2024-08-12 14:42:11 -0300

    fetching a server response

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,9 @@
-const botao = document.getElementById("script-teste");
+const limit = 15; //limite de respostas que eu quero
+const offset = 0; //paginação de pokemons
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=' + offset + '&limit=' + limit; //endereço da qual estou tirando minhas respostas
 
-botao.addEventListener("click", function(){
-    console.log('information recieved');
-});
+fetch(url).then(function (reponse){
+    console.log(reponse);
+}); //função pra pedir isso tudo e retornar minha respostas
+
+//esse é o processo de requisição pra um servidor e ele me dando uma resposta
