08c0842ff8b31b76b2e1e490a509c20975c249d2
rayssassousa22 <rayssa.sousa78@outlook.com>
2024-08-14 10:56:03 -0300

    learning to applu javascript to front end development

--- assets/javascript/poke-api.js
+++ assets/javascript/poke-api.js
@@ -25,7 +25,7 @@ pokeApi.getPokemonDetails = (pokemon) => {
 
 //funcao que vai no url e retorna as informações pro arquivo main
 pokeApi.getPokemons = () => {
-    const limit = 50; 
+    const limit = 5; 
     const offset = 0; 
     const url = 'https://pokeapi.co/api/v2/pokemon?offset=' + offset + '&limit=' + limit; 
 
